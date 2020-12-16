fd8e6e6f65b8caa6c953bdbdde727c8d74b4a3d6
Emir Savranogullari <emirsavranogullari@gmail.com>
2020-12-17 00:54:13 +0300

    Add hide prop to hide touchable

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -726,12 +726,14 @@ export default class Dropdown extends PureComponent {
 
     return (
       <View onLayout={this.onLayout} ref={this.updateContainerRef} style={containerStyle}>
-        <TouchableWithoutFeedback {...touchableProps}>
-          <View pointerEvents='box-only'>
-            {this.renderBase(props)}
-            {this.renderRipple()}
-          </View>
-        </TouchableWithoutFeedback>
+        {this.props.hide ? null : (
+          <TouchableWithoutFeedback {...touchableProps}>
+            <View pointerEvents='box-only'>
+              {this.renderBase(props)}
+              {this.renderRipple()}
+            </View>
+          </TouchableWithoutFeedback>
+        )}
 
         <Modal
           visible={modal}
